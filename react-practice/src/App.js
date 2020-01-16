7a6c4aeed6af6a7007f6eef09e0973c306534b91
nitrogendragon <dirtboy4fun@gmail.com>
2020-01-16 15:55:56 -0600

    cleanup for tutorial and starting coding

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /*This is JSx which is essentially reacts version of html
+    allows embedding components inside other components
+    Specifically this is referencing our ToDoList.js*/
+    <TodoList />
+  )
 }
 
 export default App;
