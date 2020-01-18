ae0fc1e4548ab06aa191341a1845c87e105a8961
nitrogendragon <dirtboy4fun@gmail.com>
2020-01-18 14:46:20 -0600

    added more information to practice and also a animated loading screen

--- react-pokemon-app/src/App.js
+++ react-pokemon-app/src/App.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import PokemonList from './PokemonList';
 import axios from 'axios'
 import Pagination from './Pagination';
+import './styles/appstyles.css'
 function App() {
   //set to empty array by default so we can map over it 
   const [pokemon, setPokemon] = useState([]);
   // Now our currentPage's Url is stored, can be updated, and starts as the basic page of pokeapi
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
   
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
@@ -17,17 +18,25 @@ function App() {
   useEffect(() => {
     setLoading(true)// When we make a request we are loading
     let cancel //variable for cancelling actions upon making a new axios request
-    axios.get(currentPageUrl, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(res => {
+    axios.get(
+      currentPageUrl, {cancelToken: new axios.CancelToken(c => cancel = c)}
+    ).then(res => {
       setLoading(false)// when we have our data we set loading to false
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map(p => p.name))
+      setPokemon(res.data.results.map(p => p))
     })
 
     return () => cancel() // cancels any other requests going on so we dont have a request race issue
   }, [currentPageUrl])/** every time CurrentPageUrl changes we re-render otherwise we never re-render*/
+
+
+  //Grab specific Pokemon's information
+  // useEffect(() => {
+  //   setLoading(true)
+  //   let cancel
+  //   axios.get()
+  // })
 
 
   function gotoNextPage() {
@@ -37,8 +46,12 @@ function App() {
   function gotoPrevPage() {
     setCurrentPageUrl(prevPageUrl)
   }
-
-  if (loading) return "Loading..."
+  //inline style for loading page
+  
+  if (loading) return <div class="centered-loading-screen">
+     <div>loading...</div>
+     <img src={require('./images/walking.gif')} alt="loading..."/>
+     </div>
   return ( 
     <>
     {/* Prop Format is <<Function Name> <function variable/parameter>={<variable-from-this-file>} */}
--- react-pokemon-app/src/PokemonList.js
+++ react-pokemon-app/src/PokemonList.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './styles/pokemonlist.css'
+
+
 export default function PokemonList({pokemonZ}) {
     return (
         <>
         <div className="test-grid">
-            {pokemonZ.map(p => (
-                <div class="test-grid-item" key={p}>{p}</div>
+        {pokemonZ.map(p => (
+                <div class="test-grid-item" key={p.name}>{p.name  + " " +p.url}</div>
             ))}
         </div>
-        <div></div>
+        
         </>
     )
 }
+
+
+
