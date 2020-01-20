fa7aedb3da308946a34e31ecdbf5cace1081763d
nitrogendragon <dirtboy4fun@gmail.com>
2020-01-20 01:53:58 -0600

    infinite page scrolling built

--- infinitescrollingreact/src/App.js
+++ infinitescrollingreact/src/App.js
@@ -1,7 +1,48 @@
-import React from 'react';
-
+import React, {useState, useRef, useCallback} from 'react'
+import useBookSearch from './useBookSearch'
 function App() {
-  return ( null );
-}
+  const [query, setQuery] = useState('');
+  const [pageNumber, setpageNumber] = useState(1);
+  const observer = useRef();
+
+  const {
+    books, 
+    hasMore,
+    loading,
+    error
+  }= useBookSearch(query, pageNumber);
+
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore) {
+        setpageNumber(prevPageNumber => prevPageNumber + 1);
+      }
+    });
+    if (node) observer.current.observe(node)
+    console.log(node);
+  }, [loading, hasMore]);
+
+  function handleSearch(e){
+    setQuery(e.target.value);
+    setpageNumber(1);
+  };
+
+
+  return ( 
+    <>
+    <input type="text" value ={query} onChange={handleSearch}></input>
+    {books.map((book, index) => {
+      if (books.length === index + 1)
+      {
+    return (<div ref={lastBookElementRef} key={book}>{book}</div>);
+      } else { return (<div key={book}>{book}</div>);};
+    })}
+    <div>{loading && 'Loading...'}</div>
+    <div>{error && 'Error...'}</div>
+    </>
+  );
+};
 
 export default App;
